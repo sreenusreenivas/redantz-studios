c6c6260226afdc1a363d960c9211df7daa87bdbc
Sreenu <sreenu.244@gmail.com>
2026-06-29 11:11:21 +0530

    Deployment  Fixes

--- next.config.js
+++ next.config.js
@@ -12,6 +12,18 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
